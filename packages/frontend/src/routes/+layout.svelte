bdcf86ab3b2a2367b36130238bd27e8c7b2041b4
markcirineo <markcirineo22@gmail.com>
2025-04-20 23:07:26 -0400

    Add basic light switch

--- packages/frontend/src/routes/+layout.svelte
+++ packages/frontend/src/routes/+layout.svelte
@@ -1,6 +1,26 @@
 <script lang="ts">
-	import '../app.css';
+	import "../app.css";
+	import { ModeWatcher } from "mode-watcher";
+
+	import LightSwitch from "$lib/components/ui/light-switch/light-switch.svelte";
+
 	let { children } = $props();
 </script>
 
 {@render children()}
+
+<ModeWatcher />
+
+<div class="options">
+	<LightSwitch />
+</div>
+
+<style>
+	.options {
+		position: fixed;
+		top: 1rem;
+		right: 1rem;
+		display: flex;
+		gap: 1rem;
+	}
+</style>
