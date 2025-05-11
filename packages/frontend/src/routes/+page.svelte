668128d934802a9a83733a8f9b543c3ae94ea3e0
markcirineo <markcirineo22@gmail.com>
2025-05-11 00:38:47 -0400

    Add stock list and click handling

--- packages/frontend/src/routes/+page.svelte
+++ packages/frontend/src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import SearchForm from "$lib/components/search-form/search-form.svelte";
+	import StockList from "$lib/components/stock-list/stock-list.svelte";
+	import type { SearchItem } from "$lib/types";
+
+	const list: SearchItem[] = $state([]);
+
+	const handleClicked = (item: SearchItem) => {
+		list.push(item);
+	};
 </script>
 
 <div class="flex h-screen items-center justify-center">
 	<div class="relative w-1/2">
-		<SearchForm />
+		<SearchForm clickedItem={handleClicked} />
+		<StockList {list} />
 	</div>
 </div>
