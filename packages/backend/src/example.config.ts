6f4927d8f4b7362384133ac6558d778c6c619adb
markcirineo <markcirineo22@gmail.com>
2025-11-18 22:22:12 -0500

    Update example.config.ts

--- packages/backend/src/example.config.ts
+++ packages/backend/src/example.config.ts
@@ -1,4 +1,15 @@
 export default {
 	PORT: 3000,
 	finnhub_api_key: "",
+	alpha_vantage_api_key: "",
+	jwt_secret: "",
+	jwt_expires_in: "14d",
+	db: {
+		host: "",
+		port: 5432,
+		user: "",
+		password: "",
+		database: ""
+	},
+	origin: ""
 };
